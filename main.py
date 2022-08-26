d575b2f1d0f1cfc5262810cb2e951bc7a7804fe4
AungMinKhant135 <97160958+AungMinKhant135@users.noreply.github.com>
2022-08-26 16:15:39 +0630

    Update main.py

--- main.py
+++ main.py
@@ -48,11 +48,11 @@ with col2:
     
 with col3:
     st.header("BMI")
-    #bmi = st.number_input('Insert a number')
-    #st.write('The current number is ', bmi)
+    bmi = st.number_input('Insert a number')
+    st.write('The current number is ', bmi)
     
 with col4:
-    st.header("Children")
+    st.header("Child")
     #children = st.number_input('Insert a number')
     #st.write('The current number is ', children)
     
